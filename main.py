b321cfdd85c2d163f6bd0545ef9d9207f50530ff
Capta1n-n9m0 <alievabbas1@gmail.com>
2022-02-20 03:22:13 +0400

    Few mods to main.py in hopes of fixing logging

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import Photobot
+import logging
 
+main_logger = logging.getLogger(__name__)
+main_logger.setLevel(logging.DEBUG)
 
 if __name__ == '__main__':
+    main_logger.info("Main started")
     bot = Photobot.Photobot()
     bot.run()
 
