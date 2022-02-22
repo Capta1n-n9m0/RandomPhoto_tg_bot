4adc620857ab8515ef5ce4aa7677524e96442499
Capta1n-n9m0 <alievabbas1@gmail.com>
2022-02-23 00:05:12 +0400

    Continuing moving process to SQLAlchemy

--- AlchemyDatabases.py
+++ AlchemyDatabases.py
@@ -1,19 +1,32 @@
 from sqlalchemy import create_engine, MetaData, Table, Integer, String, \
     Column, DateTime, ForeignKey, Numeric, BigInteger, Boolean
 
+import os
 from datetime import datetime
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy.orm import relationship, declarative_base, sessionmaker
 import sqlalchemy.orm as sqla_orm
 import sqlalchemy as sqla
 
 
 Base = declarative_base()
 
+CONFIG = {
+    "host": os.environ['TGBOT_DB_HOST'],
+    "user": os.environ['TGBOT_DB_USER'],
+    "password": os.environ['TGBOT_DB_PASS'],
+    "database": os.environ['TGBOT_DB_NAME'],
+}
+
+ENGINE = create_engine("mysql://%s:%s(%s):3306/%s" %
+                       (CONFIG["user"], CONFIG["password"], CONFIG["host"], CONFIG["database"]))
+
+SESSION = sessionmaker(bind=ENGINE, autoflush=True, autocommit=True)
+
 
 class User(Base):
     __tablename__ = "users"
     user_id             = Column(Integer, primary_key=True)
-    tg_id               = Column(BigInteger, nullable=True)
+    tg_id               = Column(BigInteger, nullable=False)
     username            = Column(String(32), nullable=True)
     first_name          = Column(String(64), nullable=True)
     last_name           = Column(String(64), nullable=True)
@@ -23,6 +36,14 @@ class User(Base):
     is_registered       = Column(Boolean, nullable=True)
     photos              = relationship("Photo")
     storages            = relationship("Storage")
+
+    def __repr__(self):
+        res = "<User(tg_id=%s, username=%s, first_name=%s, last_name=%s)>" % (
+            self.tg_id, self.username, self.first_name, self.last_name)
+        return res
+
+
+
 
 class Storage(Base):
     __tablename__ = "storages"
@@ -36,6 +57,12 @@ class Storage(Base):
     user_id             = Column(Integer, ForeignKey("users.user_id"))
     photos              = relationship("Photo")
 
+    def __repr__(self):
+        res = "<Storage(path=%s, type=%s, size=%s, used_space=%s)>" % (
+            self.path, self.type, self.size, self.created_date)
+        return res
+
+
 class Photo(Base):
     __tablename__ = "photos"
     photo_id            = Column(Integer, primary_key=True)
@@ -45,6 +72,11 @@ class Photo(Base):
     upload_date         = Column(DateTime, nullable=True)
     storage_id          = Column(Integer, ForeignKey("storages.storage_id"))
     user_id             = Column(Integer, ForeignKey("users.user_id"))
+
+    def __repr__(self):
+        res = "<Photo(filename=%s, size=%s)>" % (
+            self.filename, self.size)
+        return res
 
 
 
